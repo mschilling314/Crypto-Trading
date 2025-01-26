78dacee8b692b22c176ca7d787f4e79105f8e624
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2025-01-26 15:32:55 -0600

    Finish Backtesting, start Binance

--- backtest.py
+++ backtest.py
@@ -1,10 +1,11 @@
-import backtesting
+import backtesting as bt
 import pandas as pd
 
 from strategies.SimpleMovingAverage import SimpleMovingAverageStrategy
 from strategies.ExponentialMovingAverage import ExponentialMovingAverageStrategy
 from strategies.Grid import GridStrategy
 from strategies.MACD import MACDStrategy
+from strategies.VWAP import VWAPStrategy
 
 
 def scale(df, scaler=0.000001):
@@ -14,8 +15,17 @@ def scale(df, scaler=0.000001):
     return df
 
 
-data = scale(pd.read_csv("data/BTC-USD.csv"))
-backtest = backtesting.Backtest(data, MACDStrategy)
-results = backtest.run()
+def do_backtest(strat: bt.Strategy):
+    data = scale(pd.read_csv("data/BTC-USD.csv"))
+    backtest = bt.Backtest(data, strat)
+    results = backtest.run()
 
-results.to_csv("results/BTC-USD.csv")
+    results.to_csv(f"results/BTC-USD-{strat}.csv")
+
+
+if __name__=="__main__":
+    strats = [SimpleMovingAverageStrategy, ExponentialMovingAverageStrategy, GridStrategy, MACDStrategy]
+    for strat in strats:
+        do_backtest(strat=strat)
+
+
